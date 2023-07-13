f2f1472ae5e8690b6f6ef230ab465c2a7796402d
Suryakantdsa <dassuryakantadas@gmail.com>
2023-07-13 20:18:06 +0530

    my first commit and parcel bundler se up is donet

---
+++ app.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client"
+import React from "react";
+
+const AppRouter = () => {
+  return <div>hello world</div>;
+};
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppRouter/>)
